40d6b8778d89acd32ead1aa06dde0cda503bf4c3
alexscamg <alejandrosanchezsk8@gmail.com>
2024-02-05 10:20:58 +0100

    añado archivos

---
+++ mis_modulos/proyectos/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+{
+    'name': 'proyectos',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'alexsc.dev',
+    'category': 'Proyectos',
+    'description': 'Descripción larga del módulo 00',
+    'summary': 'Descripción corta del módulo 00',
+    'data': [
+        # Aquí van tus archivos XML, CSV, etc.
+    ],
+    'demo': [
+        # Datos de demostración (opcional)
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
